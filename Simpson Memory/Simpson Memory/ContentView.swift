68f64d7ab36870afa6ac841b27270988dc3a0c47
Jonathan Torres <jonaxd@icloud.com>
2022-05-15 01:14:28 -0500

    feat: dynamic board size and card height

--- Simpson Memory/Simpson Memory/ContentView.swift
+++ Simpson Memory/Simpson Memory/ContentView.swift
@@ -19,60 +19,105 @@ struct Board {
 }
 
 struct ContentView: View {
-    @State private var moves: [String] = Array(repeating: "", count: 9)
+    @State private var currentBoardConfig: Board = Board(columns: 4, rows: 4)
+    @State private var moves: [String] = Array(repeating: "", count: 16)
     @State private var difficulty: TypeBoard = .easy
     
     var body: some View {
         NavigationView {
-        VStack {
-            Picker("Choose difficulty", selection: $difficulty) {
-                Text("Easy").tag(TypeBoard.easy)
-                Text("Medium").tag(TypeBoard.medium)
-                Text("Hard").tag(TypeBoard.hard)
-            }.pickerStyle(.segmented)
-            
-            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 4), spacing: 15) {
-                ForEach(0..<moves.count, id: \.self) { index in
-                    ZStack {
-                        Color.blue
-                        Color.red
-                            .opacity(moves[index] == "" ? 1 : 0)
-                        Image("simpsons_bart1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .opacity(moves[index] != "" ? 1 : 0)
-                    }
-                    .frame(width: getWidth(), height: getHeight(), alignment: .center)
-                    .cornerRadius(15)
-                    .rotation3DEffect(
-                        .init(degrees: moves[index] != "" ? 180 : 0),
-                        axis: (x: 0.0, y: 1.0, z: 0.0),
-                        anchor: .center,
-                        anchorZ: 0.0,
-                        perspective: 1.0
-                    )
-                    .onTapGesture(perform: {
-                        withAnimation(Animation.easeIn(duration: 0.5)) {
-                            moves[index] = "X"
+            GeometryReader { proxy in
+                VStack {
+                    HStack {
+                        HStack {
+                            Image(systemName: "heart.fill")
+                                .foregroundColor(.red)
+                            Text("3")
                         }
-                    })
+                        Spacer()
+                        HStack {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.yellow)
+                            Text("1")
+                        }
+                    }
+                    Picker("Choose difficulty", selection: $difficulty) {
+                        Text("Easy").tag(TypeBoard.easy)
+                        Text("Medium").tag(TypeBoard.medium)
+                        Text("Hard").tag(TypeBoard.hard)
+                    }.pickerStyle(.segmented).onChange(of: difficulty) { _ in
+                        self.changeBoard()
+                    }
+                    
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: currentBoardConfig.columns), spacing: 15) {
+                        ForEach(0..<moves.count, id: \.self) { index in
+                            ZStack {
+                                Color.blue
+                                ZStack {
+                                    Color.gray.opacity(0.5)
+                                    Image("simpsons_smithers")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                }
+                                    .opacity(moves[index] == "" ? 1 : 0)
+                                Image("simpsons_bart1")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .opacity(moves[index] != "" ? 1 : 0)
+                            }
+                            .frame(width: getWidth(), height: getHeight(proxy), alignment: .center)
+                            .cornerRadius(15)
+                            .rotation3DEffect(
+                                .init(degrees: moves[index] != "" ? 180 : 0),
+                                axis: (x: 0.0, y: 1.0, z: 0.0),
+                                anchor: .center,
+                                anchorZ: 0.0,
+                                perspective: 1.0
+                            )
+                            .shadow(color: Color.black.opacity(0.3), radius: 2, x: 0.0, y: 0.0)
+                            .onTapGesture(perform: {
+                                withAnimation(Animation.easeIn(duration: 0.5)) {
+                                    if moves[index] == "" {
+                                        moves[index] = "X"
+                                    } else {
+                                        moves[index] = ""
+                                    }
+                                }
+                            })
+                        }
+                    }
                 }
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .padding(15)
+                .navigationTitle("Simpsons Memo")
+                .navigationBarHidden(true)
             }
-        }
-        .padding(15)
-        .navigationTitle("Simpsons Memo")
         }
     }
     
     // MARK: CALCULATE WIDTH & HEIGHT
     func getWidth() -> CGFloat {
         let width = UIScreen.main.bounds.width - (30 + 30)
-        return width / 4
+        return width / CGFloat(currentBoardConfig.columns)
     }
     
-    func getHeight() -> CGFloat {
-        let width = UIScreen.main.bounds.height - (200 + 60)
-        return width / 4
+    func getHeight(_ proxyReader: GeometryProxy) -> CGFloat {
+        let padding = currentBoardConfig.rows == 4 ? 150 : 180
+        let calculatedHeight = (proxyReader.size.height - CGFloat(padding)) / CGFloat(currentBoardConfig.rows)
+        return calculatedHeight < 0 ? 129.25 : calculatedHeight
+    }
+    
+    func changeBoard() {
+        switch difficulty {
+        case .easy:
+            currentBoardConfig = Board(columns: 4, rows: 4)
+            moves = Array(repeating: "", count: 16)
+        case .medium:
+            currentBoardConfig = Board(columns: 4, rows: 6)
+            moves = Array(repeating: "", count: 24)
+        case .hard:
+            currentBoardConfig = Board(columns: 5, rows: 6)
+            moves = Array(repeating: "", count: 30)
+        }
     }
     
 }
@@ -80,5 +125,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
